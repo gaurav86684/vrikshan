6d532a122a441291d0649cf99a210a7cdebc3eb3
gaurav86684 <46672835+gaurav86684@users.noreply.github.com>
2019-01-14 16:14:06 +0530

    finishing

---
+++ poster.php
@@ -0,0 +1,70 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <meta charset="UTF-8">
+        <title>VRIKSHAN</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+        <link rel="stylesheet" type="text/css" href="poster.css">
+
+        <style>
+            .error {color: #FF0000;}
+        </style>
+
+            
+    </head>
+    <body style="background-image: url(plants/6.jpg);">
+        <div class="box">
+            <h2>Signup</h2>
+            
+            <form action="payment.php" autocomplete="off" method="POST">
+
+                <div class="inputbox">
+                    <input type="text" name="a" required>
+                    <label>Name</lable>
+                </div>
+                <div class="inputbox">
+                    <input type="text" name="c" required>
+                    <label>Age</lable>
+                </div>
+                <div class="inputbox">
+                    <input type="text" name="d" required>
+                    <label>Phone Number  <small style="font-size: 10px;">(along with country code)</small></lable>
+                </div>
+                <div class="inputbox">
+                    <input type="email" name="e" >
+                    <label>Email(optional)</lable>
+                    
+                </div>
+                <div class="inputbox">
+                    <input type="text" name="g" required>
+                    <label>Occupation</lable>
+                    <span class="error" style="font-size: 0.4em;"> 
+                </div>
+                <input type="submit" name="submit" value="Submit">
+                <br>
+                <div class="inputbox">
+                    <label></lable>
+                    <span class="error" > </span>     
+                        <br>
+                    <span class="error" > </span>     
+                        <br>
+                    <span class="error" > </span>     
+                        <br>
+                    <span class="error" > </span>    
+                        <br>
+                    <span class="error" > </span>     
+                </div>
+            </form>
+        </div> 
+    <div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;display:block !important;'><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&amp;utm_campaign=000_logo&amp;utm_medium=website&amp;utm_content=footer_img"><img src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"  alt="www.000webhost.com"></a></div></body>
+</html>
